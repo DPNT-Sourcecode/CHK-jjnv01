7e33f41e4c3fa05be65d97504472064e77348260
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2025-01-18 19:05:05 +0000

    Sat Jan 18 19:05:05 GMT 2025

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,36 +1,60 @@
-from collections import Counter
+
+from collections import Counter, defaultdict
 from math import floor
 
 individual_prices = {
     'A':50, 
     'B':30, 
     'C':20, 
-    'D':15,
+    'D':15, 
     'E':40,
 }
 
 special_offers = {
-    'A':[(3, 130), (5,200)], 
-    'B':[(2, 45)]}
-
-whole_cart_offers = {
-    'E': (2, 'B')
+    'A':[(3, 130), (5, 200)], 
+    'B':[(2, 45)]
 }
 
-# noinspection PyUnusedLocal
-# skus = unicode string
+whole_cart_offers = {
+    'E':(2, 'B')
+}
+
 def checkout(skus):
     if not isinstance(skus, str):
         return -1
     
     items = multiitem_offers(Counter(skus))
     final_price = 0
-    try:
-        for item in items:
-            final_price+=itemcost(item, items[item])
-    except ValueError:
-        return -1
+    for item in items:
+        final_price+=itemcost(item, items[item])
     return final_price
+
+def find_best_offer(item_name, amount, all_offers):
+    best_offer_amount, best_offer_price = None, None
+    if item_name in special_offers:
+        best_offer_amount = 0
+        for i in range(len(all_offers[item_name])):
+            offer_amount = all_offers[item_name][i][0]
+            offer_price = all_offers[item_name][i][1]
+            if offer_amount<=amount and offer_amount>best_offer_amount:
+                best_offer_price = offer_price
+                best_offer_amount = offer_amount 
+    return best_offer_amount, best_offer_price
+
+# print(find_best_offer('A', 6, special_offers))
+
+def find_all_offers(item_name, amount):
+    offers = []
+    while amount>0:
+        offer = find_best_offer(item_name, amount, special_offers)
+        offer_amount = offer[0]
+        offer_price = offer[1]
+        if offer_price is not None:
+            offers.append([offer_amount, offer_price])
+            amount-=offer_amount
+        else:
+            break
+    return offers
 
 def itemcost(item_name, amount):
     discounted_price = 0
@@ -46,31 +70,6 @@ def itemcost(item_name, amount):
     final_price = amount*individual_prices[item_name] + discounted_price
     return final_price
 
-def find_best_offer(item_name,amount, all_offers):
-    best_offer_amount, best_offer_price = None, None
-    if item_name in special_offers:
-        best_offer_amount = 0 
-        for i in range(len(all_offers[item_name])):
-            offer_amount = all_offers[item_name][i][0]
-            offer_price = all_offers[item_name][i][1]
-            if offer_amount<=amount and offer_amount>best_offer_amount:
-                best_offer_price = offer_price
-                best_offer_amount = offer_amount
-    return best_offer_amount, best_offer_price
-
-def find_all_offers(item_name, amount):
-    offers = []
-    while amount>0:
-        offer = find_best_offer(item_name, amount, special_offers)
-        offer_amount = offer[0]
-        offer_price = offer[1]
-        if offer_price is not None:
-            offers.append([offer_amount, offer_price])
-            amount-=offer_amount
-        else:
-            break
-    return offers
-
 def multiitem_offers(item_counter):
     for item in item_counter:
         for offer_item in whole_cart_offers:
@@ -78,12 +77,14 @@ def multiitem_offers(item_counter):
                 free_item = whole_cart_offers[item][1]
                 required_amount = whole_cart_offers[item][0]
                 number_discounted_items = floor(item_counter[item]/required_amount)
-
+            
                 if number_discounted_items>=item_counter[free_item]:
-                    item_counter[free_item] = 0 
+                    item_counter[free_item] = 0
                 else:
-                    item_counter[free_item]-=number_discounted_items
+                    item_counter[free_item]-= number_discounted_items
+
     return item_counter
 
-    
+
+
 
--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -1,22 +1,30 @@
 import unittest
-from checkout_solution import checkout, itemcost
+from collections import Counter
+from checkout_solution import checkout, itemcost, multiitem_offers, find_all_offers
 
 class TestCheckout(unittest.TestCase):
-    def test_finalprice_checkout(self):
+    def test_checkout(self):
         self.assertEqual(checkout('B'), 30)
         self.assertEqual(checkout('BBBBB'), 120)
-        self.assertEqual(checkout('AAA'), 130)
         self.assertEqual(checkout('AAABBCD'), 130+45+20+15)
         self.assertEqual(checkout('EEB'), 80)
 
-    def test_inptfail_checkout(self):
-        self.assertEqual(checkout(3), -1)
-        self.assertEqual(checkout('1'), -1)
+        # self.assertEqual(30, checkout('B'))
     
-    def test_totaloffer_itemcost(self):
+    def test_stringfailure(self):
+        self.assertEqual(checkout(3), -1)
+
+    def test_offer_price(self):
+        # self.assertEqual(calculate_cost('B', 2), 45)
         self.assertEqual(itemcost('B', 3), 75)
         self.assertEqual(itemcost('B', 5), 90+30)
 
+    def test_find_all_offers(self):
+        self.assertEqual(find_all_offers('A', 10), [[5, 200], [5, 200]])
+
+    def test_multiitem(self):
+        expected_return = Counter('EEB')
+        self.assertEqual(multiitem_offers(Counter('EEBB')), expected_return)
 
 if __name__ == '__main__':
     unittest.main()
