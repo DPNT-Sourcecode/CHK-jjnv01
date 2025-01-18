50b67aafaa12481d89211b58564b9f96c4cbf68c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2025-01-18 19:21:55 +0000

    Sat Jan 18 19:21:55 GMT 2025

--- lib/solutions/CHK/test_checkout.py
+++ lib/solutions/CHK/test_checkout.py
@@ -17,23 +17,20 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(checkout("AAABBCD"), 130 + 45 + 20 + 15)
         self.assertEqual(checkout("EEB"), 80)
 
-        # self.assertEqual(30, checkout('B'))
 
     def test_checkout_invalid_inputs(self):
         self.assertEqual(checkout(3), -1)
         self.assertEqual(checkout("1"), -1)
         self.assertEqual(checkout("!@#"), -1)
-        self.assertEqual(checkout(""), 0)  # No items
+        self.assertEqual(checkout(""), 0)  
 
     def test_itemcost(self):
-        # self.assertEqual(calculate_cost('B', 2), 45)
         self.assertEqual(itemcost("B", 3), 75)
         self.assertEqual(itemcost("B", 5), 90 + 30)
         self.assertEqual(itemcost("A", 10), 200 + 200)
         self.assertEqual(itemcost("C", 2), 40)
 
     def test_find_best_offer(self):
-        # Test finding the best single offer
         self.assertEqual(find_best_offer("A", 6, special_offers), (5, 200))
         self.assertEqual(find_best_offer("A", 3, special_offers), (3, 130))
         self.assertEqual(find_best_offer("B", 1, special_offers), (0, None))
@@ -49,15 +46,14 @@ class TestCheckout(unittest.TestCase):
         self.assertEqual(multiitem_offers(Counter("EEBBB")), Counter({"E": 2, "B": 2}))
 
     def test_edge_cases(self):
-        # Test edge cases for various functions
-        self.assertEqual(checkout('AAABBBCCC'), 130 + 45 + 60)  # Mixed items
-        self.assertEqual(checkout(''), 0)  # Empty cart
-        self.assertRaises(ValueError, itemcost, 'X', 1)  # Invalid item
-        self.assertEqual(find_all_offers('B', 0), [])  # No items to apply offers
+        self.assertEqual(checkout("AAABBBCCC"), 130 + 75 + 60)
+        self.assertEqual(checkout(""), 0)
+        self.assertEqual(find_all_offers("B", 0), [])
 
 
 if __name__ == "__main__":
     unittest.main()
+
 
 
 
